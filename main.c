86daa6428f28041f7b1e30d0dc712092772f1309
0xp4bl0vx <0xp4bl0vx@gmail.com>
2024-11-25 12:51:45 +0100

    Added struct and functions prototypes

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+// Custom data type for tasks
+struct task {
+    int id;
+    int duration;
+    int priority;
+    int dependencies[500];
+    bool completed;
+};
+
+// Functions
+void create_tasks(struct task *tasks, int tasks_size);
+void execute_tasks(struct task *tasks, int tasks_size, int *execution_time);
+void status(struct task *tasks, int tasks_size);
+void report(struct task *tasks, int tasks_size, int execution_time);
+void menu(struct task *tasks, int tasks_size);
 
 int main(void) {
+
     return 0;
 }
